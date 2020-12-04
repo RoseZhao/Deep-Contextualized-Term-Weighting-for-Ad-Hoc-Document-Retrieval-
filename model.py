aa4c0783e003058bcf1e1b2d4b0425181faeed22
rosezhao <rosezhao@berkeley.edu>
2020-12-04 13:37:19 +0800

    add xlnet

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from transformers import BertModel
+from transformers import BertModel, XLNetModel
 
 
 class HDCTModel(BertModel):
@@ -12,6 +12,21 @@ class HDCTModel(BertModel):
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         outputs = super().forward(input_ids, attention_mask, token_type_ids)
+        last_hidden = outputs[0]    # (batch_size, seq_len, hidden_size)
+        logits = self.fc(last_hidden)    # (batch_size, seq_len, 1)
+        return logits
+
+
+class XLNETModel(XLNetModel):
+    """
+    A model wrapper around hf XLNET implementation
+    """
+    def __init__(self, config):
+        super().__init__(config=config)
+        self.fc = nn.Linear(config.hidden_size, 1)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        outputs = super().forward(input_ids=input_ids, attention_mask = attention_mask, token_type_ids = token_type_ids)
         last_hidden = outputs[0]    # (batch_size, seq_len, hidden_size)
         logits = self.fc(last_hidden)    # (batch_size, seq_len, 1)
         return logits
---
+++ run_xlnet.py
@@ -0,0 +1,172 @@
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+from transformers import (
+    AutoConfig,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+    EvalPrediction,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    set_seed,
+)
+from dataset import HDCTDataset
+from model import XLNETModel
+from runner import Trainer
+from utils import write_predictions
+from dataset import Split
+import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    data_dir: str = field(
+        metadata={"help": "The input data dir. Should contain the .txt files for a CoNLL-2003-formatted task."}
+    )
+    labels: Optional[str] = field(
+        default=None,
+        metadata={"help": "Path to a file containing all labels. If not specified, CoNLL-2003 labels are used."},
+    )
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+
+
+def main():
+    # See all possible arguments in src/transformers/training_args.py
+    # or by passing the --help flag to this script.
+    # We now keep distinct sets of args, for a cleaner separation of concerns.
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    if (
+        os.path.exists(training_args.output_dir)
+        and os.listdir(training_args.output_dir)
+        and training_args.do_train
+        and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir"
+            f"to overcome."
+        )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # Set seed
+    set_seed(training_args.seed)
+
+    # Load pretrained model and tokenizer
+    #
+    # Distributed training:
+    # The .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+    config = AutoConfig.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir, use_fast=True
+    )
+    model = HDCTModel(config)
+    if os.path.exists(model_args.model_name_or_path):
+        logging.info(f"Loading model form {model_args.model_name_or_path}")
+        model = HDCTModel.from_pretrained(model_args.model_name_or_path)
+
+
+    # get dataset
+    train_dataset = HDCTDataset(
+        data_dir=data_args.data_dir,
+        tokenizer=tokenizer,
+        model_type=model_args.model_name_or_path,
+        max_seq_length=data_args.max_seq_length) if training_args.do_train else None
+    test_dataset = HDCTDataset(
+        data_dir=data_args.data_dir,
+        tokenizer=tokenizer,
+        model_type=model_args.model_name_or_path,
+        max_seq_length=data_args.max_seq_length,
+        mode=Split.test) if training_args.do_predict else None
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_args=data_args,
+        train_dataset=train_dataset,
+        tokenizer_name=tokenizer.__class__.__name__,
+    )
+
+    # Training
+    if training_args.do_train:
+        trainer.train(
+            model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+        )
+        trainer.save_model()
+        # For convenience, we also re-save the tokenizer to the same directory,
+        if trainer.is_world_master():
+            tokenizer.save_pretrained(training_args.output_dir)
+
+    if training_args.do_predict:
+        predictions, token_ids = trainer.predict(test_dataset=test_dataset)
+        # logging.info(f"predictions = {predictions}")
+
+        write_predictions(predictions, token_ids, training_args.output_dir, len(test_dataset), tokenizer)
+
+
+if __name__=="__main__":
+    main()
