f8e73987a77196f7ca96e5dc1772a047932bf823
fangwei2 <fangweig@andrew.cmu.edu>
2020-12-04 20:47:11 -0500

    add longformer

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from transformers import BertModel, XLNetModel
+from transformers import BertModel, XLNetModel, LongformerModel
 
 
 class HDCTModel(BertModel):
@@ -30,4 +30,19 @@ class XLNETModel(XLNetModel):
         last_hidden = outputs[0]    # (batch_size, seq_len, hidden_size)
         logits = self.fc(last_hidden)    # (batch_size, seq_len, 1)
         return logits
+
+class LongformerModel(LongformerModel):
+    """
+    A model wrapper around hf longformer implementation
+    """
+    def __init__(self, config):
+        super().__init__(config=config)
+        self.fc = nn.Linear(config.hidden_size, 1)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        outputs = super().forward(input_ids=input_ids, attention_mask = attention_mask, token_type_ids = token_type_ids)
+        last_hidden = outputs[0]    # (batch_size, seq_len, hidden_size)
+        logits = self.fc(last_hidden)    # (batch_size, seq_len, 1)
+        return logits
+
 
