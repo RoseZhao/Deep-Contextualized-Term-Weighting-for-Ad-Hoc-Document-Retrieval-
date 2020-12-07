0b4b4eb76ad07f90c568ded176be28e9eb018375
fangwei2 <fangweig@andrew.cmu.edu>
2020-12-06 22:13:28 -0500

    add random seed 11875

--- sample.py
+++ sample.py
@@ -1,16 +1,19 @@
 import csv
 import random
 
+random.seed(11785)
+
 csv.field_size_limit(100000000)
-passages = set()
+passages = list()
 with open("collection.tsv", encoding='utf-8', mode="r") as f:
     rows = csv.reader(f, delimiter="\t")
     for i, row in enumerate(rows):
         qid = row[0]
-        passages.add(qid)
+        passages.append(qid)
 print("total passage num: {}".format(len(passages)))
 
-sample_passages = set(random.sample(passages, len(passages) // 10))
+random.shuffle(passages)
+sample_passages = set(passages[:len(passages) // 10])
 
 with open("collection.10%.tsv", encoding='utf-8', mode="w") as f1:
     with open("collection.tsv", encoding='utf-8', mode="r") as f:
@@ -20,7 +23,7 @@ with open("collection.10%.tsv", encoding='utf-8', mode="w") as f1:
             qid = row[0]
             if qid in sample_passages:
                 f1.write("\t".join(row) + "\n")
-                count += 1             
+                count += 1
 print("sample passage num: {}".format(count))
 
 with open("qrels.dev.10%.tsv", encoding="utf-8", mode="w") as f1:
